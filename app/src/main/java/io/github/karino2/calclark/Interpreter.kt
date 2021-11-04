9445c119fc0acf9105742025c603b6d32aa7d2d0
Kazuma Arino <hogeika2@gmail.com>
2021-11-04 22:29:18 +0900

    Add basic math func (log10, log, pow).
    Currently, just put them on global name space (similar to the situation of "from math import *").

--- app/src/main/java/io/github/karino2/calclark/Interpreter.kt
+++ app/src/main/java/io/github/karino2/calclark/Interpreter.kt
@@ -1,14 +1,69 @@
 package io.github.karino2.calclark
 
 import com.google.common.collect.ImmutableMap
+import net.starlark.java.annot.Param
+import net.starlark.java.annot.StarlarkMethod
 import net.starlark.java.eval.*
 import net.starlark.java.syntax.FileOptions
 import net.starlark.java.syntax.ParserInput
+import kotlin.math.ln
+import kotlin.math.log10
+import kotlin.math.pow
+
+class MathFunctions {
+
+    fun toDouble(x: Any) : Double? {
+        return when(x) {
+            is StarlarkInt -> {
+                x.toFiniteDouble()
+            }
+            is StarlarkFloat -> {
+                x.toDouble()
+            }
+            else -> null
+        }
+    }
+
+    @StarlarkMethod(
+        name = "log",
+        parameters = [Param(name = "x")],
+        doc = "Returns the ln of its argument."
+    )
+    fun mathLog(x: Any): StarlarkFloat {
+        return toDouble(x)?.let { StarlarkFloat.of(ln(it)) } ?: throw Starlark.errorf("Unsupported argument of log. %s", Starlark.type(x))
+    }
+
+    @StarlarkMethod(
+        name = "log10",
+        parameters = [Param(name = "x")],
+        doc = "Returns the log base 10 of its argument."
+    )
+    fun mathLog10(x: Any): StarlarkFloat {
+        return toDouble(x)?.let { StarlarkFloat.of(log10(it)) } ?: throw Starlark.errorf("Unsupported argument of log10. %s", Starlark.type(x))
+    }
+    @StarlarkMethod(
+        name = "pow",
+        parameters = [Param(name = "x"), Param(name = "y")],
+        doc = "Returns x to the power of y (x**y)."
+    )
+    fun mathPow(x: Any, y: Any): StarlarkFloat {
+        val xd = toDouble(x) ?: throw Starlark.errorf("Unsupported argument of pow x. %s", Starlark.type(x))
+        val yd = toDouble(y) ?: throw Starlark.errorf("Unsupported argument of pow y. %s", Starlark.type(y))
+        return StarlarkFloat.of(xd.pow(yd))
+    }
+}
+
+
 
 class Interpreter {
     private val fileName = "<expr>"
 
-    private fun makeEnv() : Map<String, Object> = ImmutableMap.of()
+    private fun makeEnv() : Map<String, Any> {
+        val builder = ImmutableMap.builder<String, Any>()
+        builder.put("pi", Math.PI)
+        Starlark.addMethods(builder, MathFunctions(), StarlarkSemantics.DEFAULT)
+        return builder.build()
+    }
 
     private val module : Module by lazy {
         Module.withPredeclared(StarlarkSemantics.DEFAULT, makeEnv())
